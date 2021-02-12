5b90c8f5e97beb6feae9d4e818d8d52fa1f8b373
Nadav Shani <shaninadav10@gmail.com>
2021-02-12 14:40:00 +0200

    added option to save vards in memory
    functions added: 
    1. cmpStrings - compare strings
     2. handleOneLineCommand - handles for now with assignments of two var name with two digits number
    3. handleVarAndMem - inserts var and its value to memory
    4. checkExistsVar - checks whether a var exists

--- assembly interpreter/main.asm
+++ assembly interpreter/main.asm
@@ -7,12 +7,25 @@ STACK 100h
 ;		insert value procedure
 ;		get value procedure
 
+
 DATASEG
 
+; constants
+false  equ 0
+true   equ 1
+DEBUG  equ true
 memorySize equ 500
 lineLength equ 100
+param1 equ [bp + 4]
+param2 equ [bp + 6]
+param3 equ [bp + 8]
+localVar1 equ [bp - 2]
+localVar2 equ [bp - 4]
+localVar3 equ [bp - 6]
 
-memoryVariables db memorySize dup('@')     ; buffer array - stores the data from the file
+
+memoryVariables dw memorySize dup('*')     ; buffer array - stores the data from the file
+memoryInd dw 0 ; can be up to 65,535
 
 ; variables for file opening
 filename db 'testfile.txt',0	  ; file to operate on
@@ -23,24 +36,21 @@ ErrorMsg db 'Error', 10, 13,'$'   ; error message
 buffer db lineLength dup('#')
 char db '&'
 
+
 CODESEG
 
-; constants
-param1 equ [bp + 4]
-param2 equ [bp + 6]
-param3 equ [bp + 8]
 
 ;------------------------------------------------------------------------------------------------------
 ;  Macros
 ;------------------------------------------------------------------------------------------------------
 macro newLine
 	; new line
-	MOV dl, 10		; ascii ---> 10 New Line
-	MOV ah, 02h
-	INT 21h
-	MOV dl, 13		; ascii ---> 13 Carriage Return
-	MOV ah, 02h
-	INT 21h
+	mov dl, 10		; ascii ---> 10 New Line
+	mov ah, 02h
+	int 21h
+	mov dl, 13		; ascii ---> 13 Carriage Return
+	mov ah, 02h
+	int 21h
 endm
 
 
@@ -69,7 +79,7 @@ endp OpenFile
 
 proc readLineByLine
 	
-	xor si, si				; buffer length
+	xor si, si					  	; buffer length
     read_line:
             mov ah, 3Fh      		;read file
             mov bx, [filehandle]
@@ -142,6 +152,44 @@ proc printArray
 endp printArray
 
 
+proc cmpStrings
+	push bp
+	mov bp, sp
+	
+	push si
+	push di
+	push ax
+	
+	mov dh, true  ; default return value - equal 
+	mov si, param1
+	mov di, param2
+	
+	dec si
+	keepCmp:
+		inc si
+		lodsb ; load al with next char from string 1 (si register)
+		cmp [di], al
+		jne notEq
+		
+		cmp al, 0
+		jne finishCmpStrings  ; end of string?
+		
+		jmp keepCmp
+	
+	
+	notEq:
+		xor dh, dh ; 0- not equal
+	
+	finishCmpStrings:
+		pop ax
+		pop di
+		pop si
+		
+		pop bp
+		ret 4
+endp cmpStrings
+
+
 ;------------------------------------------------------------------------------------------------------
 ; Handlers procedures - understand command 
 ;------------------------------------------------------------------------------------------------------
@@ -149,17 +197,128 @@ proc handleOneLineCommand
 	push bp
 	mov bp, sp
 	
-	mov ax, param1    			; buffer length
-	lea bx, [buffer]			; buffer offset
+	sub sp, 6 ; 3 local vars - first element, operator, second element
 	
 	push ax
 	push bx
-	call printArray
+	push si
+	push dx
 	
-	pop bp
-	ret 2
+	; print if in DEBUG mode
+	mov ah, DEBUG
+	cmp ah, 0
+	je finishHandleOneLineCommand
+	
+	; print line
+	mov ax, param1    			; buffer length
+	lea bx, [buffer]			; buffer offset
+	
+	; print array
+	push ax
+	push bx
+	call printArray
+		
+	
+	findVar:
+		; ToDO: handle 1 char var name
+		
+		; load var and value
+		mov dh, [bx]
+		mov dl, [bx + 1]
+		
+		mov ah, [bx + 3]
+		mov al, [bx + 4]
+		
+		push ax
+		push dx
+		call handleVarAndMem
+		
+	finishHandleOneLineCommand:
+		pop dx
+		pop si
+		pop bx
+		pop ax
+		
+		add sp, 6
+		pop bp
+		ret 2
 endp handleOneLineCommand
 
+
+
+;-----------------------------------------------------------
+; handle var and memory procedure
+; params: var value, var name
+;-----------------------------------------------------------
+proc handleVarAndMem
+	push bp
+	mov bp, sp
+	
+	; save registers
+	push ax
+	push bx
+	push dx
+	push si
+	
+	; get aruments
+	mov ax, param1  ; var name
+	mov dx, param2  ; var value
+	
+	; save in mem
+	lea bx, [memoryVariables]
+	mov si, [memoryInd]
+	mov [bx + si], ax	; save var name
+	add si, 2
+	mov [bx + si], dx	; save var value
+	
+	add [memoryInd], 2
+	
+	pop si
+	pop dx
+	pop bx
+	pop ax
+	
+	pop bp
+	ret 4
+endp handleVarAndMem
+
+
+
+proc checkExistsVar
+	push bp
+	mov bp, sp
+	
+	xor dh, dh	   ; default - var doesn't exists
+	mov ax, param1 ; var name
+	xor si, si
+	
+	cmp si, [memoryInd]
+	je finishCheckExistsVar
+	
+	lea bx, [memoryVariables]
+	sub si, 2
+	
+	; looping through the memoryVariables
+	keepChecking:
+		add si, 2
+		push [bx + si]
+		push ax
+		call cmpStrings
+		
+		cmp dh, true
+		je found
+		
+		cmp si, [memoryInd]
+		jne keepChecking
+	
+	; var exists
+	found:
+		mov dh, true
+		
+	finishCheckExistsVar:
+		pop bp
+		ret 2
+endp checkExistsVar
 
 ;----------------
 ; START
@@ -168,10 +327,13 @@ start:
 	mov ax, @data
 	mov ds, ax
 	
-	; Process file
 	call OpenFile
 	call readLineByLine
-	call CloseFile
+	call closeFile
+	
+	push memorySize
+	push [memoryVariables]
+	call printArray
 
 
 exit: 
